84c1cc97c28a35480ce608e870f610905c7d88b7
Bohao Wang <bj050323@gmail.com>
2020-11-26 19:15:43 +0800

    add: dijkstra

--- graph/dijkstra.cc
+++ graph/dijkstra.cc
@@ -1,9 +1,10 @@
 #include <list>
-#include <queue>
 #include <vector>
+#include <set>
 using std::vector;
 using std::list;
-using std::priority_queue;
+using std::pair;
+using std::set;
 
 typedef struct _Node {
     _Node(int s0,int e0,int w0):s(s0),e(e0),w(w0) {}
@@ -11,25 +12,19 @@ typedef struct _Node {
 } LNode;
 typedef vector<list<LNode>> Edges;
 
-struct DijkstraNodeComparator {
-    bool operator()(const LNode& a, const LNode& b) {
-        return a.w > b.w;
-    }
-};
-
 void dijk(int n, int start, const Edges& edges, int *d) {
-    priority_queue<LNode, vector<LNode>, DijkstraNodeComparator> pq;
-    vector<bool> found(n, false);
-    pq.push(LNode(start, start, 0));
-    while (!pq.empty()) {
-        LNode next = pq.top();
-        pq.pop();
-        if (found[next.e]) continue;
-        found[next.e] = true;
-        d[next.e] = next.w;
-        for (const auto& edge : edges[next.e]) {
-            if (!found[edge.e]) {
-                pq.push(LNode(0, edge.e, d[next.e] + edge.w));
+    set<pair<int,int>> st;
+    for (int i = 0; i < n; ++i) d[i] = INT_MAX;
+    st.insert({0, start});
+    d[start] = 0;
+    while (!st.empty()) {
+        int s = st.begin()->second;
+        st.erase(st.begin());
+        for (const auto& edge : edges[s]) {
+            if (d[s] + edge.w < d[edge.e]) {
+                st.erase({d[edge.e], edge.e});
+                d[edge.e] = d[s] + edge.w;
+                st.insert({d[edge.e], edge.e});
             }
         }
     }
