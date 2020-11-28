39794c8535ab9209a74747bbd6bb2e1194730c0e
Bohao <bj050323@gmail.com>
2020-11-28 17:56:03 +0800

    Create fenwick.cc

---
+++ struct/fenwick.cc
@@ -0,0 +1,22 @@
+#include <vector>
+using std::vector
+
+class FenWick {
+public:
+    FenWick(int n): a(vector<int>(n+1,0)) {}
+    // from index 1...x
+    int sum(int x) {
+        int sum = 0;
+        while (x > 0)
+            sum += a[x], x -= lsb(x);
+        return sum;
+    }
+    void add(int x, int k) {
+        int size = a.size();
+        while (x <= size) 
+            a[x] += k, x += lsb(x);
+    }
+private:
+    inline int lsb(int x) { return x & (-x); }
+    vector<int> a;
+};
