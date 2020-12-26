4b61d74480b974c0d6aadfe63e5a4dcbb6e2fecb
Bohao Wang <bj050323@gmail.com>
2020-12-26 11:04:58 +0800

    update: nCk

--- math/nCk.cc
+++ math/nCk.cc
@@ -1,14 +1,11 @@
 typedef long long ll;
 
-// 不能超过20
+// caution: overflow
 ll nCk(int n, int k) {
-    if (n < k) return 0;
-    if (n - k > k) k = n - k;
-    ll i, j;
     ll s = 1;
-    for (i = 0, j = 1; i < k; ++i) {
-        s *= (n-i);
-        for (; j <= k && (s % j == 0); ++j) s /= j;
+    for (ll d = 1; d <= k; ++d) {
+        s *= n--;
+        s /= d;
     }
     return s;
 }
