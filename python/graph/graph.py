b989923c38b7614a457a90a82a4e48a694b7f50a
BOHAO WANG <bj050323@gmail.com>
2025-12-21 13:39:57 +0800

    remove test code

--- python/graph/graph.py
+++ python/graph/graph.py
@@ -17,24 +17,3 @@ class LinkedGraph:
 
     def __iter__(self):
         return iter(self.graph)
-
-
-def main():
-    g = LinkedGraph()
-
-    g.add_edge(0, 1, 5)
-    g.add_edge(0, 5, 2)
-    g.add_edge(1, 2, 4)
-    g.add_edge(2, 3, 9)
-    g.add_edge(3, 4, 7)
-    g.add_edge(3, 5, 3)
-    g.add_edge(4, 0, 1)
-    g.add_edge(5, 4, 8)
-    g.add_edge(5, 2, 1)
-
-    for v in g:
-        for w in g.graph[v]:
-            print(f"{v} -> {w.node} = {w.weight}")
-
-
-main()
