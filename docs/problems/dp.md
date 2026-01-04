b480c6b2b82e8f7e8e065f01f955f90dbab59783
BOHAO WANG <bj050323@gmail.com>
2026-01-04 11:45:05 +0800

    update problem set

--- docs/problems/dp.md
+++ docs/problems/dp.md
@@ -8,8 +8,17 @@
 这类问题的特点是：题目给出的范围通常极大（如 $10^{18}$ 或更大），直接暴力遍历必然超时，
 而数位 DP 通过将数字拆分为每一位，利用记忆化搜索来高效求解。
 
-- [3791. 给定范围内平衡整数的数目][1]
+为什么数位 DP 这么快？比如计算数字 1XXXX 和 2XXXX 的计算，其实后面部分是重复的，利用记忆化搜索可以
+减少大量计算。时间复杂度=`O(K * State * 2 * 2)`
+
+- [LC233. 数字 1 的个数][2]
+- [LC3747. 统计移除零后不同整数的数目][3]
+- [LC2719. 统计整数数目][4]
+- [LC3791. 给定范围内平衡整数的数目][1]
 
 ## References
 
 [1]: https://leetcode.cn/problems/number-of-balanced-integers-in-a-range/description/
+[2]: https://leetcode.cn/problems/number-of-digit-one/description/
+[3]: https://leetcode.cn/problems/count-distinct-integers-after-removing-zeros/
+[4]: https://leetcode.cn/problems/count-of-integers/
--- python/dp/digit_dp.py
+++ python/dp/digit_dp.py
@@ -1,35 +1,34 @@
 """
 数位DP
-下面的例子求奇偶位和为0
+下面的例子求所有数字中包含1的个数
 """
 
 from functools import cache
 
 
 @cache
-def dfs(n_str, pos, diff, is_limit, is_num):
+def dfs(n_str, pos, state, tight, valid):
     """
     pos: 当前处理到第几位（从高到低）
     state: 状态变量（如前一位数字、数字之和等，根据题意变化）
-    is_limit: 布尔值，表示当前位是否受到上界限制
-    is_num: 布尔值，表示前几位是否填了数字（用于处理前导零）
+    tight: 布尔值，表示当前位是否受到上界限制
+    valid: 布尔值，表示前几位是否填了数字（用于处理前导零）
     """
     if pos == len(n_str):
-        return 1 if (is_num and diff == 0) else 0
+        # 判断是否满足条件
+        return state if (valid) else 0
 
     res = 0
-    # 确定当前位可以填写的数字范围
-    up = int(n_str[pos]) if is_limit else 9
+    up = int(n_str[pos]) if tight else 9
 
     for i in range(up + 1):
-        # 处理前导零
-        if not is_num and i == 0:
-            # 依然处于前导零状态，不改变 diff，pos 加 1
-            res += dfs(n_str, pos + 1, diff, is_limit and (i == up), False)
+        if not valid and i == 0:
+            # 依然处于前导零状态，不改变 state，pos 加 1
+            res += dfs(n_str, pos + 1, state, tight and (i == up), False)
         else:
-            # 正常填入数字
-            new_diff = diff + i if (pos % 2 == 0) else diff - i
-            res += dfs(n_str, pos + 1, new_diff, is_limit and (i == up), True)
+            # 更新状态
+            new_state = state + 1 if (i == 1) else state
+            res += dfs(n_str, pos + 1, new_state, tight and (i == up), True)
 
     return res
 
