379d99aa5103292f6c181387cb60b5eb47ec62ab
BOHAO WANG <bj050323@gmail.com>
2026-01-06 23:20:54 +0800

    infer matrix

--- python/math/matrix.py
+++ python/math/matrix.py
@@ -17,8 +17,31 @@ def pow_mul(m: list[list[int]], n: int, origin: list[list[int]]) -> list[list[in
     return res
 
 
+def infer_matrix(n, transformation):
+    """
+    倒推矩阵：提供n-纬度和transformation-线性向量变换函数，得到 n*n 的矩阵
+    假设递推公式 f(n) = 5 * f(n - 1) - 2 * f(n - 2)
+    现在需要构造 [f(n), f(n - 1)] = M * [f(n - 1), f(n - 2)] 里面的 M 矩阵
+    可以使用 n=2, transformation = lambda x: (5 * x[0] - 2 * x[1], x[0])
+    可以求出矩阵 ((5, -2), (1, 0))
+    transformation(1,0,0...) => 矩阵第1列
+    transformation(0,1,0...) => 矩阵第2列
+    transformation(0,0,1...) => 矩阵第n列
+    """
+
+    def one_hot(n, i):
+        vertex = [0] * n
+        vertex[i] = 1
+        return tuple(vertex)
+
+    cols = [transformation(one_hot(n, i)) for i in range(n)]
+    return tuple(zip(*cols))
+
+
 if __name__ == "__main__":
     print(mul([[1, 2, 3], [4, 5, 6]], [[1], [2], [3]]))
     # [[14], [32]]
 
     print(pow_mul([[1, 2], [3, 4]], 2, [[1], [1]]))
+
+    print(infer_matrix(2, lambda x: (5 * x[0] - 2 * x[1], x[0])))
