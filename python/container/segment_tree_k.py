935746fa1215d8b5ded115735995e42ede09788c
BOHAO WANG <bj050323@gmail.com>
2026-01-14 20:51:14 +0800

    update segment

--- python/container/segment_tree_k.py
+++ python/container/segment_tree_k.py
@@ -11,73 +11,73 @@ class SegmentTree:
         n = len(arr)
         # 容量为2的整数幂
         self.n = n
-        self.tree = [0] * (2 << (n - 1).bit_length())
-        self._build(arr, 1, 0, n - 1)
+        self.w = [0] * (2 << (n - 1).bit_length())
+        self.build(arr, 1, 0, n - 1)
 
-    def _merge_val(self, a, b):
+    def merge_val(self, a, b):
         """更新: 加法, min/max..."""
         return max(a, b)
 
-    def _maintain(self, node):
+    def maintain(self, no):
         """合并左右儿子"""
-        self.tree[node] = self._merge_val(self.tree[node * 2], self.tree[node * 2 + 1])
+        self.w[no] = self.merge_val(self.w[no * 2], self.w[no * 2 + 1])
 
-    def _build(self, arr, node, L, R):
+    def build(self, arr, no, L, R):
         if L == R:
-            self.tree[node] = arr[L]
+            self.w[no] = arr[L]
             return
         mid = (L + R) // 2
-        self._build(arr, node * 2, L, mid)
-        self._build(arr, node * 2 + 1, mid + 1, R)
-        self._maintain(node)
+        self.build(arr, no * 2, L, mid)
+        self.build(arr, no * 2 + 1, mid + 1, R)
+        self.maintain(no)
 
-    def _update(self, node, L, R, index, val):
+    def _update(self, no, L, R, index, val):
         if L == R:
-            self.tree[node] = val
+            self.w[no] = val
             return
         mid = (L + R) // 2
         if index <= mid:  # 在左边
-            self._update(node * 2, L, mid, index, val)
+            self._update(no * 2, L, mid, index, val)
         else:
-            self._update(node * 2 + 1, mid + 1, R, index, val)
-        self._maintain(node)
+            self._update(no * 2 + 1, mid + 1, R, index, val)
+        self.maintain(no)
 
     def update(self, index, val):
         """将索引 idx 的值更新为 val"""
         self._update(1, 0, self.n - 1, index, val)
 
-    def _query(self, node, L, R, ql, qr):
+    def _query(self, no, L, R, ql, qr):
         if ql <= L and R <= qr:
-            return self.tree[node]
+            return self.w[no]
         mid = (L + R) // 2
         # 剪枝优化：保证进入的递归区间必然相交
         if qr <= mid:
-            return self._query(node * 2, L, mid, ql, qr)
+            return self._query(no * 2, L, mid, ql, qr)
         if ql > mid:
-            return self._query(node * 2 + 1, mid + 1, R, ql, qr)
-        left_val = self._query(node * 2, L, mid, ql, qr)
-        right_val = self._query(node * 2 + 1, mid + 1, R, ql, qr)
-        return self._merge_val(left_val, right_val)
+            return self._query(no * 2 + 1, mid + 1, R, ql, qr)
+        left_val = self._query(no * 2, L, mid, ql, qr)
+        right_val = self._query(no * 2 + 1, mid + 1, R, ql, qr)
+        return self.merge_val(left_val, right_val)
 
     def query(self, ql, qr):
         return self._query(1, 0, self.n - 1, ql, qr)
 
-    def query_pos(self, node, L, R, ql, qr, k):
+    def query_pos(self, no, L, R, ql, qr, k):
         """查找范围内>=k的第一个位置"""
-        if self.tree[node] < k:
+        if self.w[no] < k:
             return -1
         # 到达叶子结点，必然 >= k
         if L == R:
             return L
         mid = (L + R) // 2
         if qr <= mid:
-            return self.query_pos(node * 2, L, mid, ql, qr, k)
+            return self.query_pos(no * 2, L, mid, ql, qr, k)
         if ql > mid:
-            return self.query_pos(node * 2 + 1, mid + 1, R, ql, qr, k)
-        left_val = self.query_pos(node * 2, L, mid, ql, qr, k)
+            return self.query_pos(no * 2 + 1, mid + 1, R, ql, qr, k)
+        left_val = self.query_pos(no * 2, L, mid, ql, qr, k)
         if left_val != -1:
             return left_val
-        return self.query_pos(node * 2 + 1, mid + 1, R, ql, qr, k)
+        return self.query_pos(no * 2 + 1, mid + 1, R, ql, qr, k)
 
 
 if __name__ == "__main__":
